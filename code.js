76baee3dde430be577e6e526c22343e0415b24a6
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-06-16 15:17:04 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,32 @@
+// You don't HAVE to use ANTLR to make a programming language
+let ProgrammingLanguage = (function() {
+	const ignoreReg = /[ \t\r\n;]+/g // Ignore any whitespace characters, including semicolons, since they're not important in our code.
+	function lexer(code) {
+		return code.split(ignoreReg)
+	}
+	function parser(tokens) {
+		let transpiled = []
+		let int = /[0-9]+(?!\D)/
+		let num = /[0-9]+(\.([0-9]+)?)/
+		let str = /^\"[^\"]*\"/
+		tokens.forEach(function(token) {
+			let js = {
+				token
+			}
+			if (int.test(token)) {
+				js.type = "int"
+			} else if (num.test(token)) {
+				js.type = "num"
+			} else if (str.test(token)) {
+				js.type = "str"
+			} else {
+				js.type = ""
+			}
+			transpiled.push(js)
+		})
+		return transpiled
+	}
+	return {
+		exec: (e) => parser(lexer(e))
+	}
+})()
