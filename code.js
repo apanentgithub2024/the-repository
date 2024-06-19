82fe884ff4c8b681c2e8151909386840ac35c725
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-06-19 15:33:39 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-// You don't HAVE to use ANTLR to make a programming language
+// You don't HAVE to use ANTLR to make a programming language 
 let ProgrammingLanguage = (function() {
 	const ignoreReg = /[ \t\r\n;]+/g // Ignore any whitespace characters, including semicolons, since they're not important in our code.
 	function lexer(code) {
