745ce6c0da84b61cfed3d4cd021e92b2bc2745d0
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-06-20 15:51:33 -0400

    Update interpreterbutbetter.js

--- interpreterbutbetter.js
+++ interpreterbutbetter.js
@@ -1,6 +1,5 @@
 // This shows you don't need ANTLR to make a programming language
 const run = function(text, c = true) {
-	code
 	const ret = /"((?:[^"\\]|\\.)*)"|'((?:[^"\\]|\\.)*)'|\d+|\d*\.(\d*)|\s*([\+\-\*\^]|and|or|xor|not|nand|nor|xnor|==|\^=)\s*|([a-zA-Z_][a-zA-Z_0-9]*)/
 	const keys = /(define)\s+([a-zA-Z_]([a-zA-Z_0-9]*))\s*=\s*|(delete)\s+([a-zA-Z_]([a-zA-Z_0-9]*))/
 	let tokens = new RegExp(ret.source + "|" + keys.source, "gs")
@@ -79,7 +78,7 @@ const run = function(text, c = true) {
 	}
 	const result = parser(text, lexer(text))
 	if (c) {
-		let code = ""
+		let code = "";
 		let t = ""
 		function compile(formula) {
 			const tokens = formula.f
