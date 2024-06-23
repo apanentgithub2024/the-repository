319fd91e0a489d43b077b157d1c03704b27c54b2
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-06-23 16:40:45 -0400

    Create worldwide-quiz.js

---
+++ worldwide-quiz.js
@@ -0,0 +1,122 @@
+// This includes history from less than/equal to 2024 right now!
+const questions = [
+	{
+		question: "There is technology that can tell between different smells in 2024.",
+		type: 0,
+		answers: {
+			"Yes": false,
+			"No": true
+		}
+	},
+	{
+		question: "Which organelle is the power plant of the cell?",
+		type: 0,
+		answers: {
+			"Nucleus": false,
+			"Mitochondrion": true,
+			"Endoplasmatic Reticulum": false,
+			"Golgi Body/Golgi Apparatus": false
+		}
+	},
+	{
+		question: "Which one of the objects made up the substance of oil?",
+		type: 1,
+		answers: {
+			"Trees": true,
+			"Plants": true,
+			"Metal": false,
+			"Wood": false
+		}
+	},
+	{
+		question: "Is sugar sweet, or is sugar sour?",
+		type: 0,
+		answers: {
+			"Sour": false,
+			"Sweet": true
+		}
+	},
+	{
+		question: "True or false. Most of United States' foods are banned from other countries because of food dye/coloring.",
+		type: 0,
+		answers: {
+			"Yes": true,
+			"No": false
+		}
+	},
+	{
+		question: "Do meat preservations cure cancer, or do they cause cancer, or do they increase the risk of cancer?",
+		type: 0,
+		answers: {
+			"Cure cancer": false,
+			"Cause cancer": false,
+			"Increase the risk of cancer": true
+		}
+	},
+	{
+		question: "Does too much salt overwhelm the human's taste buds?",
+		type: 0,
+		answers: {
+			"Yes": true,
+			"No": false
+		}
+	},
+	{
+		question: "True or false. Salt is the same thing/referred to as Sodium?",
+		type: 0,
+		answers: {
+			"Yes": true,
+			"No": false
+		}
+	},
+	{
+		question: "Does the human body need a lot, or a small amount of sodium?",
+		type: 0,
+		answers: {
+			"A lot": false,
+			"Small amount": true
+		}
+	},
+	{
+		question: "Claustrophobia means that the person is afraid of:",
+		type: 0,
+		answers: {
+			"Walls": false,
+			"Tight/narrow areas": true,
+			"Fruit": false,
+			"Strangers": false
+		}
+	},
+	{
+		question: "Should all lives (including bugs) be well-respected? This has a correct answer.",
+		type: 0,
+		answers: {
+			"Yes": true,
+			"No": false
+		}
+	},
+	{
+		question: "Is yogurt made with fermintation?",
+		type: 0,
+		answers: {
+			"No": false,
+			"Yes": true
+		}
+	},
+	{
+		question: "Do you think life is a simulation? This is a free choice",
+		type: 2,
+		answers: {
+			"Yes": true,
+			"No": true
+		}
+	},
+	{
+		question: "Is LeBron James a basketball player?",
+		type: 0,
+		answers: {
+			"Yes": true,
+			"No": false
+		}
+	}
+]
